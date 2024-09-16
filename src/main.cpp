5c2df222217a9bccc4694e2e54c8698caaa9c1c8
Billy Priambodo <edo.billy@gmail.com>
2024-09-16 21:06:45 +0700

    tell opengl how intepret the vertex data

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,47 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   // sizeof vertices triangle 3x3 = 9 x 4 byte = 36 byte
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  /**
+   * We need to tell opengl how to intepret the vertex data before rendering
+   * Look at the vertex buffer data image on the pdf or site learnopengl.com
+   *
+   *
+   * EXPLANATION
+   * 1. First Args 0
+   * This is specify which vertex attribute we want to configure.
+   * We already set on the vertex shader source that the layout location = 0.
+   * This sets the location of the vertex attribute to 0 and since we want to
+   * pass data to this vertex attribute, we pass in 0.
+   *
+   * 2. Second Args 3
+   * Specify the size of the vertex attribute.
+   * The vertex attribute is a vec3 so it is composed of 3 values.
+   *
+   * 3. The type of the vertex data, commonly float.
+   *
+   * 4. Specify if the data to be normalized or not.
+   * if we input integer, the integer will be normalized to 0 or 1 when
+   converted to float.
+   * We don't want this since irrelevant.
+   *
+   * 5. Stride, see the image.
+   * This tells OpenGL the spacing between consecutive vertex attributes in the
+   * buffer. Since each vertex consists of 3 floats (one for x, one for y, and
+   * one for z), the stride is 3 * sizeof(float) bytes (each float is typically
+   * 4 bytes, so stride is 12 bytes).
+   *
+   * 6. type of void* need a weird cast.
+   * This is the offset of where the position data begins in buffer.
+   * Since the position data is at the start of the data array this values is
+   just 0.
+   *
+   **/
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+
+  //  Zero => Refers to the same attribute index 0 specified in
+  //  glVertexAttribPointer(). It enables the vertex attribute at this location
+  //  so that the data will be passed to the vertex shader.
+  glEnableVertexAttribArray(0);
 
   // Var to check if shader compiled successfullly
   int success;
