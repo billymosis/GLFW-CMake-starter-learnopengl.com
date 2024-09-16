b7cee5114b7bb1aaeb2ce709d9bd90ad9c5b066f
Billy Priambodo <edo.billy@gmail.com>
2024-09-16 21:31:01 +0700

    refactor and add additional cleanup function

--- src/main.cpp
+++ src/main.cpp
@@ -24,11 +24,13 @@ const char *fragmentShaderSource = "#version 330 core\n"
 
 int main() {
   std::cout << "Hello World!" << std::endl;
+  // GLFW initialize and configure
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  // GLFW window creation
   GLFWwindow *window = glfwCreateWindow(800, 600, "Learn OPENGL", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW Window" << std::endl;
@@ -36,73 +38,13 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // GLAD load all OpenGL function pointer
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
-
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  // clang-format off
-  float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f
-  };
-  // clang-format on
-
-  GLuint VBO;
-  glGenBuffers(1, &VBO);
-  GLuint VAO;
-  glGenVertexArrays(1, &VAO);
-
-  glBindVertexArray(VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  // sizeof vertices triangle 3x3 = 9 x 4 byte = 36 byte
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  /**
-   * We need to tell opengl how to intepret the vertex data before rendering
-   * Look at the vertex buffer data image on the pdf or site learnopengl.com
-   *
-   *
-   * EXPLANATION
-   * 1. First Args 0
-   * This is specify which vertex attribute we want to configure.
-   * We already set on the vertex shader source that the layout location = 0.
-   * This sets the location of the vertex attribute to 0 and since we want to
-   * pass data to this vertex attribute, we pass in 0.
-   *
-   * 2. Second Args 3
-   * Specify the size of the vertex attribute.
-   * The vertex attribute is a vec3 so it is composed of 3 values.
-   *
-   * 3. The type of the vertex data, commonly float.
-   *
-   * 4. Specify if the data to be normalized or not.
-   * if we input integer, the integer will be normalized to 0 or 1 when
-   converted to float.
-   * We don't want this since irrelevant.
-   *
-   * 5. Stride, see the image.
-   * This tells OpenGL the spacing between consecutive vertex attributes in the
-   * buffer. Since each vertex consists of 3 floats (one for x, one for y, and
-   * one for z), the stride is 3 * sizeof(float) bytes (each float is typically
-   * 4 bytes, so stride is 12 bytes).
-   *
-   * 6. type of void* need a weird cast.
-   * This is the offset of where the position data begins in buffer.
-   * Since the position data is at the start of the data array this values is
-   just 0.
-   *
-   **/
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-
-  //  Zero => Refers to the same attribute index 0 specified in
-  //  glVertexAttribPointer(). It enables the vertex attribute at this location
-  //  so that the data will be passed to the vertex shader.
-  glEnableVertexAttribArray(0);
 
   // Var to check if shader compiled successfullly
   int success;
@@ -151,16 +93,86 @@ int main() {
   glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
   if (!success) {
     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-    std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n"
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
               << infoLog << std::endl;
   }
-
-  // Use the program
-  glUseProgram(shaderProgram);
 
   // Delete the shader object one linked to the program
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
+
+  // clang-format off
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+  };
+  // clang-format on
+
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  GLuint VBO;
+  glGenBuffers(1, &VBO);
+
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
+  // then configure vertex attributes(s).
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  // sizeof vertices triangle 3x3 = 9 x 4 byte = 36 byte
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  /**
+   * We need to tell opengl how to intepret the vertex data before rendering
+   * Look at the vertex buffer data image on the pdf or site learnopengl.com
+   *
+   *
+   * EXPLANATION
+   * 1. First Args 0
+   * This is specify which vertex attribute we want to configure.
+   * We already set on the vertex shader source that the layout location = 0.
+   * This sets the location of the vertex attribute to 0 and since we want to
+   * pass data to this vertex attribute, we pass in 0.
+   *
+   * 2. Second Args 3
+   * Specify the size of the vertex attribute.
+   * The vertex attribute is a vec3 so it is composed of 3 values.
+   *
+   * 3. The type of the vertex data, commonly float.
+   *
+   * 4. Specify if the data to be normalized or not.
+   * if we input integer, the integer will be normalized to 0 or 1 when
+   converted to float.
+   * We don't want this since irrelevant.
+   *
+   * 5. Stride, see the image.
+   * This tells OpenGL the spacing between consecutive vertex attributes in the
+   * buffer. Since each vertex consists of 3 floats (one for x, one for y, and
+   * one for z), the stride is 3 * sizeof(float) bytes (each float is typically
+   * 4 bytes, so stride is 12 bytes).
+   *
+   * 6. type of void* need a weird cast.
+   * This is the offset of where the position data begins in buffer.
+   * Since the position data is at the start of the data array this values is
+   just 0.
+   *
+   **/
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+
+  //  Zero => Refers to the same attribute index 0 specified in
+  //  glVertexAttribPointer(). It enables the vertex attribute at this location
+  //  so that the data will be passed to the vertex shader.
+  glEnableVertexAttribArray(0);
+
+  // note that this is allowed, the call to glVertexAttribPointer registered VBO
+  // as the vertex attribute's bound vertex buffer object so afterwards we can
+  // safely unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // You can unbind the VAO afterwards so other VAO calls won't accidentally
+  // modify this VAO, but this rarely happens. Modifying other VAOs requires a
+  // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
+  // VBOs) when it's not directly necessary.
+  glBindVertexArray(0);
 
   while (!glfwWindowShouldClose(window)) {
     // Input
@@ -170,7 +182,10 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // Draw triangle
     glUseProgram(shaderProgram);
+    // seeing as we only have a single VAO there's no need to bind it every
+    // time, but we'll do so to keep things a bit more organized
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -178,6 +193,16 @@ int main() {
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
+
   glfwTerminate();
   return 0;
 }
