bab49b4b2b85f581e7b182011d516808bfb52147
Billy Priambodo <edo.billy@gmail.com>
2024-09-22 21:02:35 +0700

    exercise: specular lighting

--- src/main.cpp
+++ src/main.cpp
@@ -173,6 +173,7 @@ int main() {
     lightingShader.setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
     lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
     lightingShader.setVec3("lightPos", lightPos);
+    lightingShader.setVec3("viewPos", camera.Position);
 
     // view/projection transformations
     glm::mat4 projection = glm::perspective(
