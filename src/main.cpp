f4a940f8a41679d7ee0b3a4330b9f4a8fdc63977
Billy Priambodo <edo.billy@gmail.com>
2024-09-25 07:21:21 +0700

    feat: directional light

--- src/main.cpp
+++ src/main.cpp
@@ -202,7 +202,16 @@ int main() {
   bool show_another_window = false;
   ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-  float speedFactor = 2.5f;
+  float speedFactor = 0.0f;
+
+  // multiple containers
+  glm::vec3 cubePositions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+
   // render loop
   // -----------
   while (!glfwWindowShouldClose(window)) {
@@ -237,8 +246,8 @@ int main() {
 
     // be sure to activate shader when setting uniforms/drawing objects
     lightingShader.use();
-    lightingShader.setVec3("light.position", lightPos);
     lightingShader.setVec3("viewPos", camera.Position);
+    lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
 
     // light properties
     glm::vec3 lightColor = glm::vec3(1.0f);
@@ -281,7 +290,16 @@ int main() {
 
     // render the cube
     glBindVertexArray(cubeVAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    for (unsigned int i = 0; i < 10; i++) {
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.0f * i;
+      model =
+          glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+      lightingShader.setMat4("model", model);
+
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     // also draw the lamp object
     lightCubeShader.use();
